c05fd6a64a10fc2a513be56db4b159f63bef8160
Gennady Laptev <gennadylaptev@gmail.com>
2024-12-12 19:03:00 +0500

    fixed the script

--- move_new_pdfs_from_tex_dir.sh
+++ move_new_pdfs_from_tex_dir.sh
@@ -9,8 +9,6 @@ UNTRACKED_PDFS=$(git status --porcelain | awk -v tex_dir="$TEX_DIR" '
 $1 ~ /^\?\?/ && $2 ~ /\.pdf$/ && index($2, tex_dir) == 1 { print $2 }
 ')
 
-echo "$UNTRACKED_PDFS"
-
 # Check if any untracked PDF files are found
 if [[ -z "$UNTRACKED_PDFS" ]]; then
     echo "No untracked PDF files found in $TEX_DIR."
